574a8a8995ab32b9f59063abd50a898d2595bebe
Jiansiyu <jiansiyu@gmail.com>
2017-11-22 23:24:12 -0500

    finish the tensorflow learning part, but the efficency is not very high

--- dataIO/data_preprocess.py
+++ dataIO/data_preprocess.py
@@ -17,32 +17,39 @@ class data_preprocess(object):
             self.DataBuffersize=50+1
         else:
             self.DataBuffersize=buffersize
-        self.ItemIndex=['Open','High','Low','Close','Volume','pecentage_change','range']
-    def cvsData_preprocess(self):
+        self.ItemIndex=['Open','High','Low','Close','Volume','pecentage_change','pecentage_change_0.005','range','int_range']
+        
+    def cvsData_preprocess(self,filename_in=None):
         #read the nasdaq componies name 
+        
         Stock_list_filename='googlefinance.csv'
+        if filename_in is not None:
+            Stock_list_filename=filename_in
         data_path='./'
         with open(data_path+Stock_list_filename) as Stock_list_io:
             Stock_list_lines=Stock_list_io.readlines()
         # get the information position  
+        
         self.Stock_header_infor=Stock_list_lines[0].split(',')
-        self.time_position =  self.Stock_header_infor.index('Date')
+        #self.time_position =  self.Stock_header_infor.index('Date')
         self.Open_position=self.Stock_header_infor.index('Open')
         self.High_position=self.Stock_header_infor.index('High')
         self.Low_position=self.Stock_header_infor.index('Low')
         self.Close_position=self.Stock_header_infor.index('Close')
         self.Volume_position=self.Stock_header_infor.index('Volume')
         self.pecentage_change_position=self.Stock_header_infor.index('pecentage_change')
+        self.range_position =self.Stock_header_infor.index('range')
+        self.range_position =self.Stock_header_infor.index('int_range\n')
         #range_change_position=Stock_header_infor.index('range')
         # get the individual nasdaq conpany name
         for Single_line in Stock_list_lines[1:]:
             self.Stock_infor=Single_line.split(',')
-            data_buffer_temp , training_result =self.__Databuffer(self.Stock_infor[1:])
+            data_buffer_temp , training_result =self.__Databuffer(self.Stock_infor[:],30)
             if data_buffer_temp is not None:
                 data_buffer_temp.append(training_result)
                 print(len(data_buffer_temp))
                 self.DataBuffer_all.append(data_buffer_temp)
-        self.Save_csv()
+        #self.Save_csv()
     def __Databuffer(self,singleday_data,buffer_size=None):
         '''
         
@@ -67,10 +74,14 @@ class data_preprocess(object):
         '''
         
         if len(self.DataBuffer) >= self.Buffersize_local*len(singleday_data):
-            training_result_1=(self.DataBuffer)[(self.Buffersize_local-1)*len(singleday_data)+self.ItemIndex.index('Close')]
-            training_result_2=(self.DataBuffer)[(self.Buffersize_local-2)*len(singleday_data)+self.ItemIndex.index('Close')]
-            training_result=(float(training_result_1)-float(training_result_2))/float(training_result_2)
-            return self.DataBuffer[0:-1],training_result
+            #training_result_1=(self.DataBuffer)[(self.Buffersize_local-1)*len(singleday_data)+self.ItemIndex.index('Close')]
+            #training_result_2=(self.DataBuffer)[(self.Buffersize_local-2)*len(singleday_data)+self.ItemIndex.index('Close')]
+            #training_result=(float(training_result_1)-float(training_result_2))/float(training_result_2)
+            #print(self.DataBuffer)
+            training_result=self.DataBuffer[-1]
+            #print(training_result)
+            #print(self.DataBuffer[0:-2])
+            return self.DataBuffer[0:-2],training_result
         else:
             return None, None
         
@@ -95,3 +106,8 @@ class data_preprocess(object):
 if __name__ == '__main__':
     test=data_preprocess()
     test.cvsData_preprocess()
+    test.Save_csv()
+    test_date=data_preprocess()
+    test_date.cvsData_preprocess(filename_in='stock_test.csv')
+    test_date.Save_csv('train_test.csv')
+    
