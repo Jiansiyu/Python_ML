6e0d24808597b7c56f8d35c9e1431afdb3481db8
Jiansiyu <jiansiyu@gmail.com>
2017-11-23 03:27:29 -0500

    add the training evaluation class

---
+++ training_evaluation/TrainingEvaluation.py
@@ -0,0 +1,30 @@
+'''
+@note: Training efficency evaluation
+       The initial motivation is that training the data and evaluate the new data, 
+       then put the new data back and retraining the model
+@author: Siyu
+@contact: jiansiyu@gmail.com
+          GO WAHOO !!!
+
+@version: 1.0.0
+@todo:    input data file, generate training data
+          the training data will be used for training and eveluation
+          the evaluation result will be saved in a file
+'''
+
+
+
+
+from tensor_interface.tensortraining import TensorTraining
+import csv
+
+class TrainingEvaluation(object):
+    def __init__(self):
+        '''
+        
+        '''
+
+
+if __name__ == "__main__":
+    test=TrainingEvaluation()
+    
---
+++ training_evaluation/__init__.py
@@ -0,0 +1,1 @@
+from tensor_interface.tensortraining import TensorTraining
